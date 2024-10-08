afa87d719b3389d47cecf4027425bc2c538f7036
Fady Zaafrane <fady.zaafrane@gmail.com>
2024-10-08 23:47:09 +0100

    checking

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const base = '/faderftw.github.io';
+const base = '/fadi-zaafrane';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
